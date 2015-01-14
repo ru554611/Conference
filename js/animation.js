57169d58482c25471da94ee65e72bc5fc6da4ad2
Aleksandr Mukhin <alexander.a.muhin@gmail.com>
2015-01-13 20:41:37 -0500

    Bounce
    Bounce effect was added.

--- js/animation.js
+++ js/animation.js
@@ -1,52 +1,58 @@
-function move()
-        {
-            var leftSide = document.getElementById("leftSide");
-            var rightSide = document.getElementById("rightSide");
-            var circle = document.getElementById("circle");
-            
-            //calculate width for left and right sides
-            var circleTopPosition = window.getComputedStyle(circle).top;
-            circleTopPosition = parseInt(circleTopPosition.substr(0, circleTopPosition.length - 2));
-            
-            var sideWidth = window.getComputedStyle(leftSide).width;
+$(document).ready(function(){
+                $("#circle").click(function(){
+                    
+                    var leftSide = document.getElementById("leftSide");
+                    var rightSide = document.getElementById("rightSide");
+                    var circle = document.getElementById("circle");
+                    
+                    //calculate width for left and right sides
+                    var circleTopPosition = window.getComputedStyle(circle).top;
+                    circleTopPosition = parseInt(circleTopPosition.substr(0, circleTopPosition.length - 2));
+                    
+                    docWidth = document.documentElement.clientWidth;
+                    docHeight = document.documentElement.clientHeight;
+                    
+                    var sideWidth = window.getComputedStyle(leftSide).width;
             sideWidth = parseInt(sideWidth.substr(0, sideWidth.length - 2));
-            
-            var side = sideWidth;
-            var decrementSide = side / 38;
-            
-            //if circle.style.top == 0 reverse animation
-            if (circleTopPosition == 0)
-            {
-                var circleTop = 0;
-                function frameReverse()
-                {
-                    circleTop++;
-                    leftSide.style.left = (-side + decrementSide) + 'px' ;
-                    rightSide.style.right = (-side + decrementSide) + 'px' ;
-                    circle.style.top = circleTop + '%';
-                    side = side-decrementSide;
-                    if (circleTop == 38) 
+                    var y = $('#circle').css('height');
+                    y = parseInt(y.substr(0, y.length - 2)) / 2;
+                    var x = Math.round(docHeight / 2) - y;
+                    
+                    if (circleTopPosition == 0)
                     {
-                        clearInterval(timer);
+                        $("#leftSide")
+                            .animate(
+                            { left: 0 }, 
+                            { duration: 'slow',
+                            easing: 'easeOutBounce'
+                            });
+                        $("#rightSide")
+                            .animate(
+                            { right: 0 }, 
+                            { duration: 'slow',
+                            easing: 'easeOutBounce'
+                            });
+                        $("#circle")
+                            .animate(
+                            { top: x }, 
+                            { duration: 'slow',
+                            easing: 'easeOutBounce'
+                            });
                     }
-                }
-                var timer = setInterval(frameReverse, 20);
-            }
-            else
-            {
-                var circleTop = 38;
-                function frame()
-                {
-                    side = side - decrementSide;
-                    circleTop--;
-                    leftSide.style.left = '-' + (sideWidth - side) + 'px' ;
-                    rightSide.style.right = '-' + (sideWidth - side) + 'px' ;
-                    circle.style.top = circleTop + '%';
-                    if (circleTop == 0) 
+                    else
                     {
-                        clearInterval(timer);
+                        $("#leftSide")
+                            .animate(
+                            { left: -758 }, 
+                            { duration: 'slow'});
+                        $("#rightSide")
+                            .animate(
+                            { right: -758 }, 
+                            { duration: 'slow'});
+                        $("#circle")
+                            .animate(
+                            { top: 0 }, 
+                            { duration: 'slow'});
                     }
-                }
-                var timer = setInterval(frame, 20);
-            }
-        }
+                });
+            });
